109f59b0d030eafc718772eb9e9b74ce9b4e0101
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2023-11-20 21:16:04 +0700

    create app success

--- app/src/main/java/com/soundify/MainActivity.java
+++ app/src/main/java/com/soundify/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout frameLayout;
     Toolbar toolbar;
 //Hoang Huy
+    //Tuanthanh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
