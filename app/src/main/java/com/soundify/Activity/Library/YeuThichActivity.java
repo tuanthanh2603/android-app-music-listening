b57d3290771ec1a1738092b2bbb14212105dba60
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2023-12-06 17:15:01 +0700

    commit by tuanthanh

---
+++ app/src/main/java/com/soundify/Activity/Library/YeuThichActivity.java
@@ -0,0 +1,16 @@
+package com.soundify.Activity.Library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.soundify.R;
+
+public class YeuThichActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeu_thich);
+    }
+}
